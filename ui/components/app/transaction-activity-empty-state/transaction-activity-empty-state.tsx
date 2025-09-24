89ffe3d40dea79389a039c0650e1b37a7c801125
georgewrmarshall <george.marshall@consensys.net>
2025-09-24 16:52:58 -0700

    chore: simplyfying swap route

--- ui/components/app/transaction-activity-empty-state/transaction-activity-empty-state.tsx
+++ ui/components/app/transaction-activity-empty-state/transaction-activity-empty-state.tsx
@@ -1,31 +1,12 @@
 import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { getNativeAssetForChainId } from '@metamask/bridge-controller';
+import { useSelector } from 'react-redux';
 import { twMerge } from '@metamask/design-system-react';
 import { InternalAccount } from '@metamask/keyring-internal-api';
 import { EthMethod } from '@metamask/keyring-api';
 import { TabEmptyState } from '../../ui/tab-empty-state';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import { useTheme } from '../../../hooks/useTheme';
-import { PREPARE_SWAP_ROUTE } from '../../../helpers/constants/routes';
-import { getCurrentChainId } from '../../../../shared/modules/selectors/networks';
-import {
-  getSwapsDefaultToken,
-  getCurrentKeyring,
-  getIsSwapsChain,
-  getUseExternalServices,
-} from '../../../selectors';
-import { setSwapsFromToken } from '../../../ducks/swaps/swaps';
-import { getIsMultichainAccountsState2Enabled } from '../../../selectors/multichain-accounts/feature-flags';
-import { useMultichainSelector } from '../../../hooks/useMultichainSelector';
-import { getMultichainNetwork } from '../../../selectors/multichain';
-import { MultichainNetworks } from '../../../../shared/constants/multichain/networks';
-import {
-  getIsUnifiedUIEnabled,
-  type BridgeAppState,
-} from '../../../ducks/bridge/selectors';
-import { isHardwareKeyring } from '../../../helpers/utils/hardware';
+import { getUseExternalServices } from '../../../selectors';
 import { MetaMetricsSwapsEventSource } from '../../../../shared/constants/metametrics';
 import useBridging from '../../../hooks/bridge/useBridging';
 
@@ -45,32 +26,10 @@ export const TransactionActivityEmptyState: React.FC<
 > = ({ className, account }) => {
   const t = useI18nContext();
   const theme = useTheme();
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const isSwapsChain = useSelector(getIsSwapsChain);
   const isSigningEnabled =
     account.methods.includes(EthMethod.SignTransaction) ||
     account.methods.includes(EthMethod.SignUserOperation);
   const isExternalServicesEnabled = useSelector(getUseExternalServices);
-
-  const chainId = useSelector(getCurrentChainId);
-  const defaultSwapsToken = useSelector((state) =>
-    getSwapsDefaultToken(state, chainId),
-  );
-  const keyring = useSelector(getCurrentKeyring);
-  const usingHardwareWallet = isHardwareKeyring(keyring?.type);
-
-  // Multichain selectors
-  const isMultichainAccountsState2Enabled = useSelector(
-    getIsMultichainAccountsState2Enabled,
-  );
-  const { chainId: multichainChainId } = useMultichainSelector(
-    getMultichainNetwork,
-    account,
-  );
-  const isUnifiedUIEnabled = useSelector((state: BridgeAppState) =>
-    getIsUnifiedUIEnabled(state, chainId),
-  );
 
   const { openBridgeExperience } = useBridging();
 
@@ -80,56 +39,9 @@ export const TransactionActivityEmptyState: React.FC<
       ? './images/empty-state-activity-dark.png'
       : './images/empty-state-activity-light.png';
 
-  const handleBridgeOnClick = useCallback(
-    async (isSwap: boolean) => {
-      // Handle clicking from the wallet overview page
-      openBridgeExperience(
-        MetaMetricsSwapsEventSource.MainView,
-        getNativeAssetForChainId(chainId),
-        isSwap,
-      );
-    },
-    [openBridgeExperience, chainId],
-  );
-
-  const handleSwapTokens = useCallback(async () => {
-    // Match the exact logic from coin-buttons.tsx handleSwapOnClick
-    if (isUnifiedUIEnabled) {
-      handleBridgeOnClick(true);
-      return;
-    }
-    if (multichainChainId === MultichainNetworks.SOLANA) {
-      handleBridgeOnClick(true);
-      return;
-    }
-    if (
-      isMultichainAccountsState2Enabled &&
-      chainId.toString() === MultichainNetworks.SOLANA
-    ) {
-      handleBridgeOnClick(true);
-      return;
-    }
-
-    // Regular swap flow
-    dispatch(setSwapsFromToken(defaultSwapsToken));
-    if (usingHardwareWallet) {
-      if (global.platform.openExtensionInBrowser) {
-        global.platform.openExtensionInBrowser(PREPARE_SWAP_ROUTE);
-      }
-    } else {
-      history.push(PREPARE_SWAP_ROUTE);
-    }
-  }, [
-    isUnifiedUIEnabled,
-    multichainChainId,
-    isMultichainAccountsState2Enabled,
-    chainId,
-    handleBridgeOnClick,
-    dispatch,
-    defaultSwapsToken,
-    usingHardwareWallet,
-    history,
-  ]);
+  const handleSwapOnClick = useCallback(async () => {
+    openBridgeExperience(MetaMetricsSwapsEventSource.MainView, undefined, true);
+  }, [openBridgeExperience]);
 
   return (
     <TabEmptyState
@@ -139,12 +51,9 @@ export const TransactionActivityEmptyState: React.FC<
       description={t('activityEmptyDescription')}
       actionButtonText={t('swapTokens')}
       actionButtonProps={{
-        isDisabled:
-          (!isSwapsChain && !isUnifiedUIEnabled) ||
-          !isSigningEnabled ||
-          !isExternalServicesEnabled,
+        isDisabled: !isSigningEnabled || !isExternalServicesEnabled,
       }}
-      onAction={handleSwapTokens}
+      onAction={handleSwapOnClick}
       data-testid="activity-tab-empty-state"
       className={twMerge('max-w-48', className)}
     />
